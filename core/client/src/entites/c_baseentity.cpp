Silverlan/pragma

commit b2aa959f3387c94ef81275903a473a005569b18a

- Add Entity and Entity Component remove guards to prevent potential infinite recursion - Composite component now automatically removes all of its associated entities on removal

core/client/src/entites/c_baseentity.cpp
@@ -266,8 +266,10 @@ void CBaseEntity::OnRemove()
 
 void CBaseEntity::Remove()
 {
+	if(umath::is_flag_set(GetStateFlags(),BaseEntity::StateFlags::Removed))
+		return;
+	BaseEntity::Remove(); // Has to be called first!
 	SceneRenderDesc::AssertRenderQueueThreadInactive();
-	BaseEntity::Remove();
 	Game *game = client->GetGameState();
 	game->RemoveEntity(this);
 }

core/client/src/entites/c_entities.cpp
@@ -56,6 +56,8 @@ CBaseEntity *CGame::CreateEntity(std::string classname)
 
 void CGame::RemoveEntity(BaseEntity *ent)
 {
+	if(umath::is_flag_set(ent->GetStateFlags(),BaseEntity::StateFlags::Removed))
+		return;
 	if(ent->IsPlayer())
 		m_numPlayers--;
 	unsigned int cIdx = static_cast<CBaseEntity*>(ent)->GetClientIndex();

core/server/src/entities/s_baseentity.cpp
@@ -178,7 +178,9 @@ pragma::NetEventId SBaseEntity::RegisterNetEvent(const std::string &name) const
 
 void SBaseEntity::Remove()
 {
-	BaseEntity::Remove();
+	if(umath::is_flag_set(GetStateFlags(),BaseEntity::StateFlags::Removed))
+		return;
+	BaseEntity::Remove(); // Has to be called first!
 	Game *game = server->GetGameState();
 	game->RemoveEntity(this);
 }

core/server/src/entities/s_entities.cpp
@@ -60,6 +60,8 @@ SBaseEntity *SGame::CreateEntity(std::string classname)
 
 void SGame::RemoveEntity(BaseEntity *ent)
 {
+	if(umath::is_flag_set(ent->GetStateFlags(),BaseEntity::StateFlags::Removed))
+		return;
 	auto *s_ent = static_cast<SBaseEntity*>(ent);
 	if(s_ent->IsShared())
 	{

core/shared/src/entities/baseentity.cpp
@@ -300,7 +300,7 @@ pragma::BasePhysicsComponent *BaseEntity::GetPhysicsComponent() const {return m_
 pragma::BaseModelComponent *BaseEntity::GetModelComponent() const {return m_modelComponent;}
 pragma::BaseGenericComponent *BaseEntity::GetGenericComponent() const {return m_genericComponent;}
 
-void BaseEntity::Remove() {}
+void BaseEntity::Remove() {umath::set_flag(m_stateFlags,StateFlags::Removed);}
 void BaseEntity::RemoveSafely() {GetNetworkState()->GetGameState()->ScheduleEntityForRemoval(*this);}
 
 void BaseEntity::InitializeHandle() {BaseEntity::InitializeHandle<EntityHandle>();}

core/shared/src/entities/components/composite_component.cpp
@@ -32,6 +32,17 @@ void CompositeComponent::Initialize()
 	BaseEntityComponent::Initialize();
 }
 
+void CompositeComponent::OnRemove()
+{
+	BaseEntityComponent::OnRemove();
+	for(auto &hEnt : m_ents)
+	{
+		if(!hEnt.IsValid())
+			continue;
+		hEnt->Remove();
+	}
+}
+
 std::vector<EntityHandle>::const_iterator CompositeComponent::FindEntity(BaseEntity &ent) const
 {
 	return std::find_if(m_ents.begin(),m_ents.end(),[&ent](const EntityHandle &hEnt) {

core/shared/include/pragma/entities/baseentity.h
@@ -90,7 +90,8 @@ public:
 		CollisionBoundsChanged = RotationChanged<<1u,
 		RenderBoundsChanged = CollisionBoundsChanged<<1u,
 
-		HasWorldComponent = RenderBoundsChanged<<1u
+		HasWorldComponent = RenderBoundsChanged<<1u,
+		Removed = HasWorldComponent<<1u
 	};
 
 	static void RegisterEvents(pragma::EntityComponentManager &componentManager);

core/shared/include/pragma/entities/components/base_entity_component.hpp
@@ -72,7 +72,8 @@ namespace pragma
 		{
 			None = 0u,
 			IsThinking = 1u,
-			IsLogicEnabled = IsThinking<<1u
+			IsLogicEnabled = IsThinking<<1u,
+			Removed = IsLogicEnabled<<1u
 		};
 
 		BaseEntityComponent(const BaseEntityComponent&)=delete;

commit a71b60830bb272c5373de0ac15552ae15ea4c9c4

- Fix issue introduced with b2aa959 where entities could not get removed anymore - Add DirectoryChangeListener Lua binding

core/client/src/entites/c_baseentity.cpp
@@ -268,7 +268,7 @@ void CBaseEntity::Remove()
 {
 	if(umath::is_flag_set(GetStateFlags(),BaseEntity::StateFlags::Removed))
 		return;
-	BaseEntity::Remove(); // Has to be called first!
+	BaseEntity::Remove();
 	SceneRenderDesc::AssertRenderQueueThreadInactive();
 	Game *game = client->GetGameState();
 	game->RemoveEntity(this);

core/client/src/entites/c_entities.cpp
@@ -58,6 +58,7 @@ void CGame::RemoveEntity(BaseEntity *ent)
 {
 	if(umath::is_flag_set(ent->GetStateFlags(),BaseEntity::StateFlags::Removed))
 		return;
+	ent->SetStateFlag(BaseEntity::StateFlags::Removed);
 	if(ent->IsPlayer())
 		m_numPlayers--;
 	unsigned int cIdx = static_cast<CBaseEntity*>(ent)->GetClientIndex();

core/server/src/entities/s_baseentity.cpp
@@ -180,7 +180,7 @@ void SBaseEntity::Remove()
 {
 	if(umath::is_flag_set(GetStateFlags(),BaseEntity::StateFlags::Removed))
 		return;
-	BaseEntity::Remove(); // Has to be called first!
+	BaseEntity::Remove();
 	Game *game = server->GetGameState();
 	game->RemoveEntity(this);
 }

core/server/src/entities/s_entities.cpp
@@ -62,6 +62,7 @@ void SGame::RemoveEntity(BaseEntity *ent)
 {
 	if(umath::is_flag_set(ent->GetStateFlags(),BaseEntity::StateFlags::Removed))
 		return;
+	ent->SetStateFlag(BaseEntity::StateFlags::Removed);
 	auto *s_ent = static_cast<SBaseEntity*>(ent);
 	if(s_ent->IsShared())
 	{

core/shared/include/pragma/lua/sh_lua_component.hpp
@@ -98,6 +98,8 @@ namespace pragma
 		std::any GetMemberValue(const MemberInfo &memberInfo) const;
 		void SetMemberValue(const MemberInfo &memberInfo,const std::any &value) const;
 		const std::vector<MemberInfo> &GetMembers() const;
+		virtual void OnEntityComponentAdded(BaseEntityComponent &component) override;
+		virtual void OnEntityComponentRemoved(BaseEntityComponent &component) override;
 		std::unordered_map<pragma::NetEventId,CallbackHandle> m_boundNetEvents;
 
 		struct NetworkedMemberInfo

core/shared/include/pragma/lua/sh_lua_component_t.hpp
@@ -145,6 +145,8 @@ namespace Lua
 		classDef.def("OnEntitySpawn",&LuaBaseEntityComponentWrapper::OnEntitySpawn,&LuaBaseEntityComponentWrapper::default_OnEntitySpawn);
 		classDef.def("OnAttachedToEntity",&LuaBaseEntityComponentWrapper::OnAttachedToEntity,&LuaBaseEntityComponentWrapper::default_OnAttachedToEntity);
 		classDef.def("OnDetachedFromEntity",&LuaBaseEntityComponentWrapper::OnDetachedToEntity,&LuaBaseEntityComponentWrapper::default_OnDetachedToEntity);
+		// classDef.def("OnEntityComponentAdded",&LuaBaseEntityComponentWrapper::OnEntityComponentAdded,&LuaBaseEntityComponentWrapper::default_OnEntityComponentAdded);
+		// classDef.def("OnEntityComponentRemoved",&LuaBaseEntityComponentWrapper::OnEntityComponentRemoved,&LuaBaseEntityComponentWrapper::default_OnEntityComponentRemoved);
 		// HandleEvent is variadic and can't be defined like this in luabind!
 		//classDef.def("HandleEvent",&LuaBaseEntityComponentWrapper::HandleEvent,&LuaBaseEntityComponentWrapper::default_HandleEvent);
 		classDef.def("Save",&LuaBaseEntityComponentWrapper::Save,&LuaBaseEntityComponentWrapper::default_Save);

core/shared/include/pragma/lua/sh_lua_component_wrapper.hpp
@@ -47,6 +47,12 @@ public:
 
 	void Load(DataStream ds,uint32_t version) {}
 	static void default_Load(lua_State *l,LuaBaseEntityComponentWrapper &hComponent,DataStream ds,uint32_t version) {}
+
+	void OnEntityComponentAdded(LuaBaseEntityComponentWrapper &hComponent) {}
+	static void default_OnEntityComponentAdded(lua_State *l,LuaBaseEntityComponentWrapper &hComponent) {}
+
+	void OnEntityComponentRemoved(LuaBaseEntityComponentWrapper &hComponent) {}
+	static void default_OnEntityComponentRemoved(lua_State *l,LuaBaseEntityComponentWrapper &hComponent) {}
 };
 
 #endif

core/shared/src/entities/baseentity.cpp
@@ -300,7 +300,7 @@ pragma::BasePhysicsComponent *BaseEntity::GetPhysicsComponent() const {return m_
 pragma::BaseModelComponent *BaseEntity::GetModelComponent() const {return m_modelComponent;}
 pragma::BaseGenericComponent *BaseEntity::GetGenericComponent() const {return m_genericComponent;}
 
-void BaseEntity::Remove() {umath::set_flag(m_stateFlags,StateFlags::Removed);}
+void BaseEntity::Remove() {}
 void BaseEntity::RemoveSafely() {GetNetworkState()->GetGameState()->ScheduleEntityForRemoval(*this);}
 
 void BaseEntity::InitializeHandle() {BaseEntity::InitializeHandle<EntityHandle>();}

core/shared/src/entities/components/composite_component.cpp
@@ -39,7 +39,7 @@ void CompositeComponent::OnRemove()
 	{
 		if(!hEnt.IsValid())
 			continue;
-		hEnt->Remove();
+		hEnt->RemoveSafely();
 	}
 }
 

core/shared/src/lua/class.cpp
@@ -61,6 +61,8 @@
 #include <luabind/out_value_policy.hpp>
 #include <mathutil/inverse_kinematics/ik.hpp>
 #include <mathutil/inverse_kinematics/constraints.hpp>
+#include <sharedutils/magic_enum.hpp>
+#include <fsys/directory_watcher.h>
 
 extern DLLNETWORK Engine *engine;
 
@@ -118,6 +120,39 @@ std::ostream &operator<<(std::ostream &out,const ALSound &snd)
 
 static void RegisterLuaMatrices(Lua::Interface &lua);
 static void RegisterIk(Lua::Interface &lua);
+
+static void register_directory_watcher(luabind::module_ &modUtil)
+{
+	auto defListener = luabind::class_<DirectoryWatcherCallback>("DirectoryChangeListener");
+	defListener.add_static_constant("LISTENER_FLAG_NONE",umath::to_integral(DirectoryWatcherCallback::WatchFlags::None));
+	defListener.add_static_constant("LISTENER_FLAG_BIT_WATCH_SUB_DIRECTORIES",umath::to_integral(DirectoryWatcherCallback::WatchFlags::WatchSubDirectories));
+	defListener.add_static_constant("LISTENER_FLAG_ABSOLUTE_PATH",umath::to_integral(DirectoryWatcherCallback::WatchFlags::AbsolutePath));
+	defListener.add_static_constant("LISTENER_FLAG_START_DISABLED",umath::to_integral(DirectoryWatcherCallback::WatchFlags::StartDisabled));
+	defListener.add_static_constant("LISTENER_FLAG_WATCH_DIRECTORY_CHANGES",umath::to_integral(DirectoryWatcherCallback::WatchFlags::WatchDirectoryChanges));
+	static_assert(magic_enum::flags::enum_count<DirectoryWatcherCallback::WatchFlags>() == 4);
+	defListener.scope[luabind::def("create",static_cast<std::shared_ptr<DirectoryWatcherCallback>(*)(lua_State*,const std::string&,luabind::object)>([](lua_State *l,const std::string &path,luabind::object callback) {
+		Lua::CheckFunction(l,2);
+		return std::make_shared<DirectoryWatcherCallback>(path,[callback](const std::string &fileName) mutable {
+			callback(fileName);
+		},DirectoryWatcherCallback::WatchFlags::None);
+	}))];
+	defListener.scope[luabind::def("create",static_cast<std::shared_ptr<DirectoryWatcherCallback>(*)(lua_State*,const std::string&,luabind::object,DirectoryWatcherCallback::WatchFlags)>([](lua_State *l,const std::string &path,luabind::object callback,DirectoryWatcherCallback::WatchFlags flags) {
+		Lua::CheckFunction(l,2);
+		return std::make_shared<DirectoryWatcherCallback>(path,[callback](const std::string &fileName) mutable {
+			callback(fileName);
+		},flags);
+	}))];
+	defListener.def("Poll",static_cast<uint32_t(*)(lua_State*,DirectoryWatcherCallback&)>([](lua_State *l,DirectoryWatcherCallback &listener) {
+		return listener.Poll();
+	}));
+	defListener.def("SetEnabled",static_cast<void(*)(lua_State*,DirectoryWatcherCallback&,bool)>([](lua_State *l,DirectoryWatcherCallback &listener,bool enabled) {
+		listener.SetEnabled(enabled);
+	}));
+	defListener.def("IsEnabled",static_cast<bool(*)(lua_State*,DirectoryWatcherCallback&)>([](lua_State *l,DirectoryWatcherCallback &listener) {
+		return listener.IsEnabled();
+	}));
+	modUtil[defListener];
+}
 
 static Quat QuaternionConstruct() {return uquat::identity();}
 static Quat QuaternionConstruct(float w,float x,float y,float z) {return Quat(w,x,y,z);}
@@ -252,6 +287,7 @@ void NetworkState::RegisterSharedLuaClasses(Lua::Interface &lua)
 	});
 
 	auto &modUtil = lua.RegisterLibrary("util");
+	register_directory_watcher(modUtil);
 
 	auto defParallelJob = luabind::class_<util::BaseParallelJob>("ParallelJob");
 	defParallelJob.add_static_constant("JOB_STATUS_FAILED",umath::to_integral(util::JobStatus::Failed));

core/shared/src/lua/sh_lua_component.cpp
@@ -754,6 +754,17 @@ static void read_value(Game &game,udm::LinkedPropertyWrapper &udm,std::any &valu
 	}
 }
 
+void BaseLuaBaseEntityComponent::OnEntityComponentAdded(BaseEntityComponent &component)
+{
+	BaseEntityComponent::OnEntityComponentAdded(component);
+	// CallLuaMember<void,luabind::object>("OnEntityComponentAdded",component.GetLuaObject()); // Unused: Already covered by EVENT_ON_COMPONENT_ADDED
+}
+void BaseLuaBaseEntityComponent::OnEntityComponentRemoved(BaseEntityComponent &component)
+{
+	BaseEntityComponent::OnEntityComponentRemoved(component);
+	// CallLuaMember<void,luabind::object>("OnEntityComponentRemoved",component.GetLuaObject()); // Unused: Already covered by EVENT_ON_COMPONENT_REMOVED
+}
+
 const std::vector<BaseLuaBaseEntityComponent::MemberInfo> &BaseLuaBaseEntityComponent::GetMembers() const {return m_members;}
 void BaseLuaBaseEntityComponent::Save(udm::LinkedPropertyWrapper &udm)
 {
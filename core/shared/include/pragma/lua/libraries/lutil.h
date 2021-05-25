Silverlan/pragma

commit 385d90f5c0980d353bcacc054360d44d22a2e45a

- Composite component now ensures child entities are in the same scene(s) as their parents - lua_exec and lua_exec_cl console commands now cache the Lua files by default so they're auto-reloaded in the future whenever they're changed (Add "nocache" argument to disable) - Fix various rare crashes - Fix incorrect links in readme - If no occlusion culler is available for the scene, all renderable entities are now being rendered

core/shared/include/pragma/lua/libraries/lutil.h
@@ -17,7 +17,7 @@ namespace Lua
 	DLLNETWORK void set_ignore_include_cache(bool b);
 	namespace global
 	{
-		DLLNETWORK luabind::object include(lua_State *l,const std::string &f,bool ignoreCache);
+		DLLNETWORK luabind::object include(lua_State *l,const std::string &f,bool ignoreCache,bool reload);
 		DLLNETWORK luabind::object include(lua_State *l,const std::string &f);
 		DLLNETWORK luabind::object exec(lua_State *l,const std::string &f);
 		DLLNETWORK std::string get_script_path();

core/shared/src/lua/libraries/lutil.cpp
@@ -77,13 +77,13 @@ void Lua::util::register_library(lua_State *l)
 	];
 }
 
-luabind::object Lua::global::include(lua_State *l,const std::string &f) {return include(l,f,s_bIgnoreIncludeCache);}
+luabind::object Lua::global::include(lua_State *l,const std::string &f) {return include(l,f,s_bIgnoreIncludeCache,false);}
 
-luabind::object Lua::global::include(lua_State *l,const std::string &f,bool ignoreCache)
+luabind::object Lua::global::include(lua_State *l,const std::string &f,bool ignoreCache,bool reload)
 {
 	auto *lInterface = engine->GetLuaInterface(l);
 	std::vector<std::string> *includeCache = (lInterface != nullptr) ? &lInterface->GetIncludeCache() : nullptr;
-	auto fShouldInclude = [includeCache,ignoreCache](std::string fpath) -> bool {
+	auto fShouldInclude = [includeCache,ignoreCache,reload](std::string fpath) -> bool {
 		if(includeCache == nullptr)
 			return true;
 		if(fpath.empty() == false)
@@ -97,9 +97,10 @@ luabind::object Lua::global::include(lua_State *l,const std::string &f,bool igno
 		auto it = std::find_if(includeCache->begin(),includeCache->end(),[fpath](const std::string &other) {
 			return ustring::compare(fpath,other,false);
 		});
-		if(it != includeCache->end())
+		if(!reload && it != includeCache->end())
 			return ignoreCache;
-		includeCache->push_back(fpath);
+		if(it == includeCache->end())
+			includeCache->push_back(fpath);
 		return true;
 	};
 	auto *nw = engine->GetNetworkState(l);

core/client/src/c_engine_cmd.cpp
@@ -23,6 +23,7 @@
 #include <image/prosper_render_target.hpp>
 #include <shader/prosper_shader_blur.hpp>
 #include <prosper_window.hpp>
+#include <fsys/directory_watcher.h>
 
 extern DLLCLIENT void debug_render_stats(bool enabled,bool full,bool print,bool continuous);
 void CEngine::RegisterConsoleCommands()
@@ -35,10 +36,15 @@ void CEngine::RegisterConsoleCommands()
 		Game *game = state->GetGameState();
 		if(game == NULL)
 			return;
-		Lua::set_ignore_include_cache(true);
-			auto fname = argv.at(0);
-			game->ExecuteLuaFile(fname);
-		Lua::set_ignore_include_cache(false);
+		auto fname = argv.at(0);
+		if(argv.size() > 1 && argv[1] == "nocache")
+		{
+			Lua::set_ignore_include_cache(true);
+				game->ExecuteLuaFile(fname);
+			Lua::set_ignore_include_cache(false);
+			return;
+		}
+		Lua::global::include(state->GetLuaState(),fname,false,true);
 	},ConVarFlags::None,"Opens and executes a lua-file on the client.",[](const std::string &arg,std::vector<std::string> &autoCompleteOptions) {
 		std::vector<std::string> resFiles;
 		auto path = "lua\\" +arg;

core/client/src/entites/c_baseentity.cpp
@@ -46,6 +46,7 @@
 #include <pragma/entities/components/base_transform_component.hpp>
 #include <pragma/util/util_handled.hpp>
 #include <pragma/entities/components/base_physics_component.hpp>
+#include <pragma/entities/components/composite_component.hpp>
 #include <pragma/entities/entity_component_system_t.hpp>
 
 LINK_ENTITY_TO_CLASS(entity,CBaseEntity);
@@ -70,6 +71,19 @@ void CBaseEntity::OnComponentAdded(pragma::BaseEntityComponent &component)
 		m_modelComponent = &static_cast<pragma::CModelComponent&>(component);
 	else if(typeid(component) == typeid(pragma::CGenericComponent))
 		m_genericComponent = &static_cast<pragma::CGenericComponent&>(component);
+	else if(typeid(component) == typeid(pragma::CompositeComponent))
+	{
+		static_cast<pragma::CompositeComponent&>(component).AddEventCallback(pragma::CompositeComponent::EVENT_ON_ENTITY_ADDED,[this](std::reference_wrapper<pragma::ComponentEvent> e) -> util::EventReply {
+			auto &evData = static_cast<pragma::CECompositeEntityChanged&>(e.get());
+			static_cast<CBaseEntity&>(evData.ent).GetSceneFlagsProperty()->Link(*GetSceneFlagsProperty()); // TODO: This skips the EVENT_ON_SCENE_FLAGS_CHANGED event
+			return util::EventReply::Unhandled;
+		});
+		static_cast<pragma::CompositeComponent&>(component).AddEventCallback(pragma::CompositeComponent::EVENT_ON_ENTITY_REMOVED,[this](std::reference_wrapper<pragma::ComponentEvent> e) -> util::EventReply {
+			auto &evData = static_cast<pragma::CECompositeEntityChanged&>(e.get());
+			static_cast<CBaseEntity&>(evData.ent).GetSceneFlagsProperty()->Unlink(); // TODO: This skips the EVENT_ON_SCENE_FLAGS_CHANGED event
+			return util::EventReply::Unhandled;
+		});
+	}
 }
 void CBaseEntity::OnComponentRemoved(pragma::BaseEntityComponent &component)
 {

core/client/src/entites/components/c_model_component.cpp
@@ -353,6 +353,11 @@ RenderMeshGroup &CModelComponent::GetLodRenderMeshGroup(uint32_t lod)
 	UpdateRenderMeshes();
 	lod = umath::min(lod,static_cast<uint32_t>(m_lodRenderMeshGroups.size() -1));
 	assert(lod < m_lodRenderMeshGroups.size());
+	if(lod >= m_lodRenderMeshGroups.size())
+	{
+		static RenderMeshGroup emptyGroup {};
+		return emptyGroup; // TODO: This should only be returned as const!
+	}
 	return m_lodRenderMeshGroups[lod];
 }
 const RenderMeshGroup &CModelComponent::GetLodRenderMeshGroup(uint32_t lod) const {return const_cast<CModelComponent*>(this)->GetLodRenderMeshGroup(lod);}

core/client/src/rendering/scene/scene_render_desc.cpp
@@ -734,14 +734,6 @@ void SceneRenderDesc::BuildRenderQueues(const util::DrawSceneInfo &drawSceneInfo
 
 		if(umath::is_flag_set(drawSceneInfo.renderFlags,FRender::Dynamic))
 		{
-			// Some entities are exempt from occlusion culling altogether, we'll handle them here
-			for(auto *pRenderComponent : pragma::CRenderComponent::GetEntitiesExemptFromOcclusionCulling())
-			{
-				if(ShouldConsiderEntity(static_cast<CBaseEntity&>(pRenderComponent->GetEntity()),m_scene,drawSceneInfo.renderFlags) == false)
-					continue;
-				AddRenderMeshesToRenderQueue(drawSceneInfo,*pRenderComponent,m_scene,cam,vp,nullptr);
-			}
-
 			if(stats)
 				t = std::chrono::steady_clock::now();
 
@@ -749,8 +741,28 @@ void SceneRenderDesc::BuildRenderQueues(const util::DrawSceneInfo &drawSceneInfo
 			auto *culler = m_scene.FindOcclusionCuller();
 			if(culler)
 			{
+				// Some entities are exempt from occlusion culling altogether, we'll handle them here
+				for(auto *pRenderComponent : pragma::CRenderComponent::GetEntitiesExemptFromOcclusionCulling())
+				{
+					if(ShouldConsiderEntity(static_cast<CBaseEntity&>(pRenderComponent->GetEntity()),m_scene,drawSceneInfo.renderFlags) == false)
+						continue;
+					AddRenderMeshesToRenderQueue(drawSceneInfo,*pRenderComponent,m_scene,cam,vp,nullptr);
+				}
+
 				auto &dynOctree = culler->GetOcclusionOctree();
 				CollectRenderMeshesFromOctree(drawSceneInfo,dynOctree,m_scene,cam,vp,drawSceneInfo.renderFlags,frustumPlanes,&bspTrees,&bspLeafNodes);
+			}
+			else
+			{
+				// No occlusion culler available; We'll have to iterate ALL renderable entities
+				EntityIterator entIt {*c_game};
+				entIt.AttachFilter<TEntityIteratorFilterComponent<pragma::CRenderComponent>>();
+				for(auto *ent : entIt)
+				{
+					if(ShouldConsiderEntity(*static_cast<CBaseEntity*>(ent),m_scene,drawSceneInfo.renderFlags) == false)
+						continue;
+					AddRenderMeshesToRenderQueue(drawSceneInfo,*static_cast<CBaseEntity*>(ent)->GetRenderComponent(),m_scene,cam,vp,nullptr);
+				}
 			}
 
 			if(stats)

core/shared/src/engine_cmd.cpp
@@ -205,9 +205,15 @@ void Engine::RegisterConsoleCommands()
 		Game *game = state->GetGameState();
 		if(game == NULL)
 			return;
-		Lua::set_ignore_include_cache(true);
-			game->ExecuteLuaFile(argv[0]);
-		Lua::set_ignore_include_cache(false);
+		auto fname = argv.at(0);
+		if(argv.size() > 1 && argv[1] == "nocache")
+		{
+			Lua::set_ignore_include_cache(true);
+				game->ExecuteLuaFile(fname);
+			Lua::set_ignore_include_cache(false);
+			return;
+		}
+		Lua::global::include(state->GetLuaState(),fname,false,true);
 	},ConVarFlags::None,"Opens and executes a lua-file on the server.",[](const std::string &arg,std::vector<std::string> &autoCompleteOptions) {
 		std::vector<std::string> resFiles;
 		auto path = "lua\\" +arg;

core/shared/src/entities/components/composite_component.cpp
@@ -39,7 +39,8 @@ void CompositeComponent::OnRemove()
 	{
 		if(!hEnt.IsValid())
 			continue;
-		hEnt->RemoveSafely();
+		//hEnt->RemoveSafely();
+		hEnt->Remove();
 	}
 }
 

commit 6297cfd66a139077bc9c612622e30cdf9b97504f

Fix compilation issue introduced with 385d90f

core/shared/include/pragma/lua/libraries/lutil.h
@@ -18,6 +18,7 @@ namespace Lua
 	namespace global
 	{
 		DLLNETWORK luabind::object include(lua_State *l,const std::string &f,bool ignoreCache,bool reload);
+		DLLNETWORK luabind::object include(lua_State *l,const std::string &f,bool ignoreCache);
 		DLLNETWORK luabind::object include(lua_State *l,const std::string &f);
 		DLLNETWORK luabind::object exec(lua_State *l,const std::string &f);
 		DLLNETWORK std::string get_script_path();

core/shared/src/lua/libraries/lutil.cpp
@@ -78,6 +78,7 @@ void Lua::util::register_library(lua_State *l)
 }
 
 luabind::object Lua::global::include(lua_State *l,const std::string &f) {return include(l,f,s_bIgnoreIncludeCache,false);}
+luabind::object Lua::global::include(lua_State *l,const std::string &f,bool ignoreCache) {return include(l,f,ignoreCache,false);}
 
 luabind::object Lua::global::include(lua_State *l,const std::string &f,bool ignoreCache,bool reload)
 {
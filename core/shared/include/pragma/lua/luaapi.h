59307429d20606382a3f2aab1e7326edf63630d2
Silverlan <silverlann@gmail.com>
2021-07-25 06:50:48 +0200

    Lua: Add pseudo types for overload resolution and LAD signature detection

--- core/shared/include/pragma/lua/luaapi.h
+++ core/shared/include/pragma/lua/luaapi.h
@@ -16,8 +16,122 @@
 #include <luabind/luabind.hpp>
 #include <luabind/operator.hpp>
 
+namespace luabind
+{
+	// Additional types for overload resolution
+	namespace adl {
+		template <typename T>
+		struct optional : object
+		{
+			optional(const object &o)
+				: object(o)
+			{}
+		};
+
+		template <typename T>
+		struct tableT : object
+		{
+			tableT(const object &o)
+				: object(o)
+			{}
+		};
+
+		template <typename T,typename T2>
+		struct tableTT : object
+		{
+			tableTT(const object &o)
+				: object(o)
+			{}
+		};
+
+		template <typename ...T>
+		struct variant : object
+		{
+			variant(const object &o)
+				: object(o)
+			{}
+		};
+
+		template <typename ...T>
+			struct mult : object
+		{
+			mult(const object &o)
+				: object(o)
+			{}
+			mult(lua_State *l,T... args)
+				: object()
+			{
+				([&] (auto & input)
+				{
+					Lua::Push(l,input);
+				} (args), ...);
+			}
+		};
+
+	} // namespace adl
+
+	using adl::optional;
+	using adl::tableT;
+	using adl::tableTT;
+	using adl::variant;
+	using adl::mult;
+
+	template <typename T>
+	struct lua_proxy_traits<adl::optional<T> >
+		: lua_proxy_traits<object>
+	{
+		static bool check(lua_State* L, int idx)
+		{
+			return lua_proxy_traits<object>::check(L, idx);
+		}
+	};
+
+	template <typename T>
+	struct lua_proxy_traits<adl::tableT<T> >
+		: lua_proxy_traits<object>
+	{
+		static bool check(lua_State* L, int idx)
+		{
+			return lua_proxy_traits<object>::check(L, idx);
+		}
+	};
+
+	template <typename T,typename T2>
+	struct lua_proxy_traits<adl::tableTT<T,T2> >
+		: lua_proxy_traits<object>
+	{
+		static bool check(lua_State* L, int idx)
+		{
+			return lua_proxy_traits<object>::check(L, idx);
+		}
+	};
+
+	template <typename ...T>
+	struct lua_proxy_traits<adl::variant<T...> >
+		: lua_proxy_traits<object>
+	{
+		static bool check(lua_State* L, int idx)
+		{
+			return lua_proxy_traits<object>::check(L, idx);
+		}
+	};
+
+	template <typename ...T>
+	struct lua_proxy_traits<adl::mult<T...> >
+		: lua_proxy_traits<object>
+	{
+		static bool check(lua_State* L, int idx)
+		{
+			return lua_proxy_traits<object>::check(L, idx);
+		}
+	};
+};
+
 namespace Lua
 {
+	extern const luabind::object nil;
+	using object = luabind::object;
+
 	class Interface;
 	DLLNETWORK void initialize_lua_state(Lua::Interface &lua);
 	DLLNETWORK void set_extended_lua_modules_enabled(bool b);
--- core/shared/src/lua/engine_lua.cpp
+++ core/shared/src/lua/engine_lua.cpp
@@ -17,6 +17,8 @@ static auto s_bExtendedModules = false;
 void Lua::set_extended_lua_modules_enabled(bool b) {s_bExtendedModules = b;}
 bool Lua::get_extended_lua_modules_enabled() {return s_bExtendedModules;}
 
+DLLNETWORK const Lua::object Lua::nil {};
+
 void Lua::initialize_lua_state(Lua::Interface &lua)
 {
 	// See http://www.lua.org/source/5.3/linit.c.html

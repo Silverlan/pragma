4e529f400b96f0e1dbce686d326334531487cb01
Silverlan <silverlann@gmail.com>
2021-09-18 20:18:53 +0200

    Fix incorrect camera component nearZ member default value

--- core/shared/src/entities/environment/env_camera.cpp
+++ core/shared/src/entities/environment/env_camera.cpp
@@ -42,7 +42,7 @@ void BaseEnvCameraComponent::RegisterMembers(pragma::EntityComponentManager &com
 			T,float,
 			static_cast<void(T::*)(float)>(&T::SetNearZ),
 			static_cast<float(T::*)() const>(&T::GetFarZ)
-		>("nearz",DEFAULT_FAR_Z,AttributeSpecializationType::Distance);
+		>("nearz",DEFAULT_NEAR_Z,AttributeSpecializationType::Distance);
 		memberInfo.SetMin(0.1f);
 		memberInfo.updateDependenciesFunction = [](BaseEntityComponent &component,std::vector<std::string> &outAffectedProps) {
 			auto &c = static_cast<T&>(component);

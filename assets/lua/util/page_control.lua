8b3ebf065d906e2f604724ff97b68634951cab01
Silverlan <silverlann@gmail.com>
2021-11-05 20:56:48 +0100

    Add util.PageControl Lua class

---
+++ assets/lua/util/page_control.lua
@@ -0,0 +1,61 @@
+--[[
+    Copyright (C) 2021 Silverlan
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+]]
+
+util.register_class("util.PageControl")
+function util.PageControl:__init(maxItemCount)
+	self.m_maxItemCount = maxItemCount or 6
+	self.m_itemCount = 0
+	self:SetPage(0)
+end
+function util.PageControl:SetItemCount(n)
+	self.m_itemCount = n
+	self:UpdatePage()
+end
+function util.PageControl:SetMaxItemCount(n)
+	self.m_maxItemCount = n
+	self:UpdatePage()
+end
+function util.PageControl:UpdatePage(page)
+	page = page or self:GetPage()
+	local numPages = self:GetPageCount()
+	if(numPages == 0) then
+		self.m_page = 0
+		return
+	end
+	self.m_page = math.min(page,numPages -1)
+end
+function util.PageControl:SetPage(page) self:UpdatePage(page) end
+function util.PageControl:GetItemCount() return self.m_itemCount end
+function util.PageControl:GetMaxItemCount() return self.m_maxItemCount end
+function util.PageControl:GetPage() return self.m_page end
+function util.PageControl:GetItemOffset()
+	return self.m_page *self:GetMaxItemCount()
+end
+function util.PageControl:GetPageCount() return math.ceil((self:GetItemCount() -1) /self:GetMaxItemCount()) end
+function util.PageControl:IsItemVisible(id)
+	local offset = self:GetItemOffset()
+	return id >= offset and id < offset +self:GetMaxItemCount()
+end
+function util.PageControl:GoToNextPage()
+	local numPages = self:GetPageCount()
+	if(numPages == 0) then
+		self:SetPage(0)
+		return
+	end
+	self:SetPage((self.m_page +1) %self:GetPageCount())
+end
+function util.PageControl:GoToPreviousPage()
+	local numPages = self:GetPageCount()
+	if(numPages == 0) then
+		self:SetPage(0)
+		return
+	end
+	local page = self.m_page -1
+	if(page < 0) then page = self:GetPageCount() -1 end
+	self:SetPage(page)
+end

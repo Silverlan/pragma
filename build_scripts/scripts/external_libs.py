6a7684bed8b2571c610316faf223a2d51422a0bb
Silverlan <silverlann@gmail.com>
2023-06-09 05:28:31 +0200

    build: update mathutil submodule

--- build_scripts/scripts/external_libs.py
+++ build_scripts/scripts/external_libs.py
@@ -13,7 +13,7 @@ get_submodule("datasystem","https://github.com/Silverlan/datasystem.git","a6dfff
 get_submodule("iglfw","https://github.com/Silverlan/iglfw.git","4b9cf5c8f7f5e38e84c7e8529f956cd35897401e")
 get_submodule("luasystem","https://github.com/Silverlan/luasystem.git","e22584368ce0f9a5c0c223b8a632d968377d1ae3")
 get_submodule("materialsystem","https://github.com/Silverlan/materialsystem.git","6214d38")
-get_submodule("mathutil","https://github.com/Silverlan/mathutil.git","83dce221b0ef2217a6326576966ebb2177f4deb2")
+get_submodule("mathutil","https://github.com/Silverlan/mathutil.git","5aa9ec46b104b086ddfd090d1be0030c9f2788b5")
 get_submodule("networkmanager","https://github.com/Silverlan/networkmanager.git","68d2f5c")
 get_submodule("panima","https://github.com/Silverlan/panima.git","481d115e3c7190720474e6bf202939feba1015c1")
 get_submodule("prosper","https://github.com/Silverlan/prosper.git","eddf6a9")

709017586cbf34e555fc0ef526d05ba459f95646
SlawekNowy <38943477+SlawekNowy@users.noreply.github.com>
2023-08-11 19:03:48 +0200

    Revert "Fixed missing symbol by explicitely instantiating it."
    This reverts commit 6d55ea7805af674e48654253e707f8aafde4e0cd.

--- core/client/src/entities/components/renderers/rasterization/prosper_shader_t_init.cpp
+++
@@ -1,9 +0,0 @@
-#if defined(__linux) &&defined(__clang__)
-//this file is included solely beacuse some symbols never get emitted.
-
-#include <shader/prosper_shader.hpp>
-#include <shader/prosper_shader_blur.hpp>
-#include <shader/prosper_shader_t.hpp>
-
-template const std::shared_ptr<prosper::IRenderPass> &prosper::ShaderGraphics::GetRenderPass<prosper::ShaderBlurBase>(prosper::IPrContext &, uint32_t );
-#endif

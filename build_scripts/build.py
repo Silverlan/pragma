b21e0c2266648ddd32a887c5fadb3497790e0e05
Silverlan <silverlann@gmail.com>
2023-10-03 17:57:01 +0200

    build: update submodule "filmmaker"

--- build_scripts/build.py
+++ build_scripts/build.py
@@ -960,7 +960,7 @@ def download_addon(name,addonName,url,commitId=None):
 
 curDir = os.getcwd()
 if with_pfm:
-	download_addon("PFM","filmmaker","https://github.com/Silverlan/pfm.git","5ab836790a6eff7cb15c07960bc34675aef0cae9")
+	download_addon("PFM","filmmaker","https://github.com/Silverlan/pfm.git","dfc18a3aef10e5292b6c1ce62787d8d6cbf05fff")
 	download_addon("model editor","tool_model_editor","https://github.com/Silverlan/pragma_model_editor.git","362981334d7b2f023dbcb1a2d1972fdc843b15e7")
 
 if with_vr:

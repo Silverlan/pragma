640f779c2ec9fd7eb551da0bdaccd8b17ea485bf
Silverlan <silverlann@gmail.com>
2023-10-11 17:03:16 +0200

    build: update pr_curl submodule

--- build_scripts/scripts/modules.py
+++ build_scripts/scripts/modules.py
@@ -10,7 +10,7 @@ modules_dir = curDir +"/modules"
 os.chdir(modules_dir)
 get_submodule("interfaces","https://github.com/Silverlan/pragma_interfaces.git","874295b")
 get_submodule("pr_audio_dummy","https://github.com/Silverlan/pr_audio_dummy.git","282dd1a")
-get_submodule("pr_curl","https://github.com/Silverlan/pr_curl.git","5b91868")
+get_submodule("pr_curl","https://github.com/Silverlan/pr_curl.git","150e9d0c3d5116745fb83a7da28eb32666125347")
 get_submodule("pr_prosper_opengl","https://github.com/Silverlan/pr_prosper_opengl.git","a268347")
 get_submodule("pr_prosper_vulkan","https://github.com/Silverlan/pr_prosper_vulkan.git","87a235e503550d431f862cfc6fe7916596406201")
 

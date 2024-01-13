43c050774d401445b93b7f9949ff0980336568d8
SlawekNowy <38943477+SlawekNowy@users.noreply.github.com>
2024-01-13 10:22:50 +0100

    Try to fix only one path in rpath

--- build_scripts/cmake/patch_libraries.sh
+++ build_scripts/cmake/patch_libraries.sh
@@ -4,7 +4,7 @@ lib_dir="$root_dir/lib"
 
 patch_library() {
     if [ -e $1 ]; then
-	    patchelf --set-rpath '$ORIGIN' $1
+	    patchelf --set-rpath '$ORIGIN' $1;
 	    patchelf --add-rpath '$ORIGIN/lib' $1
     fi;
 }
@@ -15,7 +15,7 @@ patch_module() {
     if [ -e $1 ]; then
         #https://stackoverflow.com/a/28523143
         relative_path=$(realpath --relative-to="$(dirname "$1")" "$lib_dir")
-	    patchelf --set-rpath '$ORIGIN' $1
+	    patchelf --set-rpath '$ORIGIN' $1;
 	    patchelf --add-rpath "\$ORIGIN/$relative_path" $1
     fi;
 }

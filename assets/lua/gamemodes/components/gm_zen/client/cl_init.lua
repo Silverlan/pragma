0198f0fbfa34e3c6d9a47c6acebd0f1bbc3ee15f
Silverlan <silverlann@gmail.com>
2024-01-28 15:29:10 +0100

    feat(zen): disable default HUD

--- assets/lua/gamemodes/components/gm_zen/client/cl_init.lua
+++ assets/lua/gamemodes/components/gm_zen/client/cl_init.lua
@@ -3,6 +3,12 @@ include("cl_hud.lua")
 local Component = ents.GmZen
 
 function Component:InitializeHud()
-	local p = gui.create("WIZenHUD")
+	--[[local p = gui.create("WIZenHUD")
 	p:SetSize(400, 200)
+
+	self.m_hudZen = p]]
+end
+
+function Component:OnRemove()
+	--util.remove(self.m_hudZen)
 end

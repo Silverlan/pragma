488e318854490a80b53fb361b45d189517f6ad3c
Sławomir Śpiewak <sosw.slawomir.spiewak@gmail.com>
2024-04-13 15:02:16 +0200

    Update build.py

--- build_scripts/build.py
+++ build_scripts/build.py
@@ -758,7 +758,7 @@ if with_pfm:
         )
         add_pragma_module(
             name="pr_unirender",
-            commitSha="54fb3e251a22c81d57413cec95f95bdee51522dc",
+            commitSha="0be8f2b4f9edaed6b098118ce1a7e4cf91b39870",
             repositoryUrl="https://github.com/Slaweknowy/pr_cycles.git"
         )
         add_pragma_module(

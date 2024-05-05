64b9c4c27734ca373faa49e00e5f9b281326c2b4
Silverlan <silverlann@gmail.com>
2024-05-05 06:57:37 +0200

    build: update submodule "vkvparser"

--- build_scripts/scripts/third_party_libs.py
+++ build_scripts/scripts/third_party_libs.py
@@ -39,6 +39,6 @@ get_submodule("zlib","https://github.com/Silverlan/zlib.git","0dcc7471231d72376d
 get_submodule("fmt","https://github.com/fmtlib/fmt.git","a337011")
 get_submodule("cppbezierfit","https://github.com/Silverlan/cppbezierfit.git","2a3eea546f4d07494b2d6793ec863be8d35c023f")
 get_submodule("mpaheader","https://github.com/SlawekNowy/mpaheader.git","2618c39")
-get_submodule("vkvparser","https://github.com/Silverlan/VKVParser.git","f1a6661a1d3dd6ba77d2a5f09c3a099533b1a98d")
+get_submodule("vkvparser","https://github.com/Silverlan/VKVParser.git","c1ec619cebd05e0be083f7a604761278185586d2")
 
 os.chdir(curDir)

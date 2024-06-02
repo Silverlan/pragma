e742f76669fcecec7008f07d705b3833138ff042
Silverlan <silverlann@gmail.com>
2024-06-02 13:05:51 +0200

    revert: undo change in 073d944

--- tools/pfm_executable/src/main.cpp
+++ tools/pfm_executable/src/main.cpp
@@ -8,7 +8,6 @@
 #include "pragma/pragma_executable.hpp"
 
 int main(int argc, char *argv[])
-{
 try {
 	std::vector<std::string> extraArgs {
 	  "-title",
@@ -26,14 +25,12 @@ try {
 	auto hModule = pragma::launch_pragma(cargs.size(), cargs.data());
 	return hModule ? EXIT_SUCCESS : EXIT_FAILURE;
 }
-catch(const std::exception &e) //it would not work if you pass by value
-{
+catch(...) {
 	// Note: Calling std::current_exception in a std::set_terminate handler will return NULL due to a bug in the VS libraries.
 	// Catching all unhandled exceptions here and then calling the handler works around that issue.
+	std::get_terminate()();
+}
 
-	std::get_terminate();
-}
-}
 #ifdef _WIN32
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) { return main(__argc, __argv); }
 #endif

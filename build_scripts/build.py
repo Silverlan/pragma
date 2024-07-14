067168760ed3bfe67edb503ceb7e9ca610481ddc
Silverlan <silverlann@gmail.com>
2024-07-14 20:32:38 +0200

    ci(run_common_tests): add "create_actor" pfm test

--- build_scripts/build.py
+++ build_scripts/build.py
@@ -1070,7 +1070,7 @@ def download_addon(name,addonName,url,commitId=None):
 curDir = os.getcwd()
 if not skip_repository_updates:
 	if with_pfm:
-		download_addon("PFM","filmmaker","https://github.com/Silverlan/pfm.git","f352f782622e0948f068b380d00a678d25ac3bee")
+		download_addon("PFM","filmmaker","https://github.com/Silverlan/pfm.git","951a526ba2d12e59a488a03dfb5799aad905b639")
 		download_addon("model editor","tool_model_editor","https://github.com/Silverlan/pragma_model_editor.git","56d46dacb398fa7540e794359eaf1081c9df1edd")
 
 	if with_vr:

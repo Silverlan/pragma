1bf4be4da16ea9b25cc90c16ab9b37487731b77e
Silverlan <silverlann@gmail.com>
2024-10-18 11:34:42 +0200

    build(linux): update clang to version 19.1.2

--- build_scripts/build.py
+++ build_scripts/build.py
@@ -187,10 +187,10 @@ mkpath(tools)
 if platform == "linux":
 	curDir = os.getcwd()
 	os.chdir(deps_dir)
-	clang19_root = os.getcwd() +"/LLVM-19.1.1-Linux-X64"
+	clang19_root = os.getcwd() +"/LLVM-19.1.2-Linux-X64"
 	if not Path(clang19_root).is_dir():
 		print_msg("Downloading clang-19...")
-		http_extract("https://github.com/llvm/llvm-project/releases/download/llvmorg-19.1.1/LLVM-19.1.1-Linux-X64.tar.xz",format="tar.xz")
+		http_extract("https://github.com/llvm/llvm-project/releases/download/llvmorg-19.1.2/LLVM-19.1.2-Linux-X64.tar.xz",format="tar.xz")
 	c_compiler = clang19_root +"/bin/clang"
 	cxx_compiler = clang19_root +"/bin/clang++"
 	print_msg("Setting c_compiler override to '" +c_compiler +"'")

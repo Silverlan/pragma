1a5044b581a6c19c04fb2cc51955c0c160f44768
Silverlan <silverlann@gmail.com>
2024-10-19 11:16:56 +0200

    build: fix incorrect commit sha

--- build_scripts/scripts/modules.py
+++ build_scripts/scripts/modules.py
@@ -11,7 +11,7 @@ os.chdir(modules_dir)
 get_submodule("interfaces","https://github.com/Silverlan/pragma_interfaces.git","48c1b84f2245324e90871924e4f606f846197818")
 get_submodule("pr_audio_dummy","https://github.com/Silverlan/pr_audio_dummy.git","1a806a1a7b2283bd8551d07e4f1d680499f68b90")
 get_submodule("pr_curl","https://github.com/Silverlan/pr_curl.git","974c67cc76710809a9595fcfbc4167554799cd7f")
-get_submodule("pr_prosper_opengl","https://github.com/Silverlan/pr_prosper_opengl.git","ff5b2b91e82aca8a2bcc35542691f4488b12f0af")
+get_submodule("pr_prosper_opengl","https://github.com/Silverlan/pr_prosper_opengl.git","f0c4b6800bded2f5b218fcb49e8460d28c8ceff6")
 get_submodule("pr_prosper_vulkan","https://github.com/Silverlan/pr_prosper_vulkan.git","932a3596e24383accdb03d9f2c16ea5dc5c2ef5e")
 
 os.chdir(curDir)

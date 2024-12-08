b9b060b62f7b8bd386c3c45fd80f6a5a06e66adb
Silverlan <silverlann@gmail.com>
2024-12-08 20:29:42 +0100

    feat: crashdumps now contain more memory information

--- core/shared/src/debug/mdump.cpp
+++ core/shared/src/debug/mdump.cpp
@@ -75,6 +75,8 @@ CrashHandler::CrashHandler(const std::string &appName) : m_appName {appName}
 CrashHandler::~CrashHandler() { g_crashHandler = nullptr; }
 
 #ifdef _WIN32
+BOOL CALLBACK MyMiniDumpCallback(PVOID pParam, const PMINIDUMP_CALLBACK_INPUT pInput, PMINIDUMP_CALLBACK_OUTPUT pOutput);
+
 std::optional<std::string> CrashHandler::GenerateMiniDump(std::string &outErr) const
 {
 	if(!m_pExceptionInfo) {
@@ -131,14 +133,95 @@ std::optional<std::string> CrashHandler::GenerateMiniDump(std::string &outErr) c
 	ExInfo.ExceptionPointers = m_pExceptionInfo;
 	ExInfo.ClientPointers = NULL;
 
+	MINIDUMP_CALLBACK_INFORMATION mci;
+
+	mci.CallbackRoutine = (MINIDUMP_CALLBACK_ROUTINE)MyMiniDumpCallback;
+	mci.CallbackParam = 0;
+
 	// write the dump
-	//MiniDumpWithFullMemory
-	BOOL bOK = pDump(GetCurrentProcess(), GetCurrentProcessId(), hFile, MiniDumpNormal, &ExInfo, NULL, NULL);
+	// Add MiniDumpWithDataSegs if global variables are needed
+	MINIDUMP_TYPE mdt = (MINIDUMP_TYPE)(MiniDumpWithIndirectlyReferencedMemory | MiniDumpScanMemory);
+	BOOL bOK = pDump(GetCurrentProcess(), GetCurrentProcessId(), hFile, mdt, &ExInfo, NULL, &mci);
 	::CloseHandle(hFile);
 	if(bOK)
 		return szDumpPath;
 	outErr = "Failed to write minidump!";
 	return {};
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Custom minidump callback
+//
+
+BOOL CALLBACK MyMiniDumpCallback(PVOID pParam, const PMINIDUMP_CALLBACK_INPUT pInput, PMINIDUMP_CALLBACK_OUTPUT pOutput)
+{
+	BOOL bRet = FALSE;
+
+	// Check parameters
+
+	if(pInput == 0)
+		return FALSE;
+
+	if(pOutput == 0)
+		return FALSE;
+
+	// Process the callbacks
+
+	switch(pInput->CallbackType) {
+	case IncludeModuleCallback:
+		{
+			// Include the module into the dump
+			bRet = TRUE;
+		}
+		break;
+
+	case IncludeThreadCallback:
+		{
+			// Include the thread into the dump
+			bRet = TRUE;
+		}
+		break;
+
+	case ModuleCallback:
+		{
+			// Does the module have ModuleReferencedByMemory flag set ?
+
+			if(!(pOutput->ModuleWriteFlags & ModuleReferencedByMemory)) {
+				// No, it does not - exclude it
+
+				pOutput->ModuleWriteFlags &= (~ModuleWriteModule);
+			}
+
+			bRet = TRUE;
+		}
+		break;
+
+	case ThreadCallback:
+		{
+			// Include all thread information into the minidump
+			bRet = TRUE;
+		}
+		break;
+
+	case ThreadExCallback:
+		{
+			// Include this information
+			bRet = TRUE;
+		}
+		break;
+
+	case MemoryCallback:
+		{
+			// We do not include any information here -> return FALSE
+			bRet = FALSE;
+		}
+		break;
+
+	case CancelCallback:
+		break;
+	}
+
+	return bRet;
 }
 #endif
 

0bd322c07114661470d556305cf9b9d5e299a24b
Silverlan <silverlann@gmail.com>
2024-12-16 21:33:34 +0100

    build: update submodule "util_shadergraph"

--- build_scripts/scripts/external_libs.py
+++ build_scripts/scripts/external_libs.py
@@ -32,7 +32,7 @@ get_submodule("util_udm","https://github.com/Silverlan/util_udm.git","9b44008857
 get_submodule("util_versioned_archive","https://github.com/Silverlan/util_versioned_archive.git","e8db74a6ad9ed44d7e8a4492eb8fe44eae3452f6")
 get_submodule("util_vmf","https://github.com/Silverlan/util_vmf.git","4e477b1425e1a7b9898975effaf527f661e792da")
 get_submodule("util_zip","https://github.com/Silverlan/util_zip.git","d9bf05a5cbf71bf53f9fbea82c7352f870989ed1")
-get_submodule("util_shadergraph","https://github.com/Silverlan/util_shadergraph.git","6cf0701347097efc567aa71cd49eb4728eff1901")
+get_submodule("util_shadergraph","https://github.com/Silverlan/util_shadergraph.git","c85ff99d55facd5a4814bfbc1bb6c699f07f0f7d")
 get_submodule("vfilesystem","https://github.com/Silverlan/vfilesystem.git","a150f67ab21993715d116bc31faaa2da5923e37e")
 get_submodule("wgui","https://github.com/Silverlan/wgui.git","ca084a2aecdcdc017d09ae14faf82b0255950de6")
 get_submodule("util_unicode","https://github.com/Silverlan/util_unicode.git","5a0ac6c02f199e42d7d38d99231503cf42e26f8a")

5642cdd5f198519983bec303a73181f34f8aab5a
Silverlan <silverlann@gmail.com>
2024-12-27 21:16:33 +0100

    build: update submodules "util_shadergraph", "filmmaker"

--- build_scripts/build.py
+++ build_scripts/build.py
@@ -1141,7 +1141,7 @@ def download_addon(name,addonName,url,commitId=None):
 curDir = os.getcwd()
 if not skip_repository_updates:
 	if with_pfm:
-		download_addon("PFM","filmmaker","https://github.com/Silverlan/pfm.git","b6d16232c33a0c424acd48ff6994acfd51d86ad8")
+		download_addon("PFM","filmmaker","https://github.com/Silverlan/pfm.git","072a5c6bc9218136e539d13b4aaf73b228ea094d")
 		download_addon("model editor","tool_model_editor","https://github.com/Silverlan/pragma_model_editor.git","a9ea4820f03be250bdf1e6951dad313561b75b17")
 
 	if with_vr:

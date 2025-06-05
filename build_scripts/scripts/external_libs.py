56478eec5e4b541975889d86acf71f9ac98072b4
Silverlan <silverlann@gmail.com>
2025-06-05 07:43:29 +0200

    build: update submodule "vfilesystem"

--- build_scripts/scripts/external_libs.py
+++ build_scripts/scripts/external_libs.py
@@ -33,7 +33,7 @@ get_submodule("util_versioned_archive","https://github.com/Silverlan/util_versio
 get_submodule("util_vmf","https://github.com/Silverlan/util_vmf.git","6dedf4a8543544f1d8d4d2376b9b14cd1afd913a")
 get_submodule("util_zip","https://github.com/Silverlan/util_zip.git","80bc70a6ec0d832bf56081f7d55d3dfd550dae51")
 get_submodule("util_shadergraph","https://github.com/Silverlan/util_shadergraph.git","15231d501ca893d077c1a1de5cd173daf94fc1e5")
-get_submodule("vfilesystem","https://github.com/Silverlan/vfilesystem.git","7534a6f31a04695275b7cfed6a1d75763806b0c1")
+get_submodule("vfilesystem","https://github.com/Silverlan/vfilesystem.git","8b42066924d018738ee1acf62edd115d9665eced")
 get_submodule("wgui","https://github.com/Silverlan/wgui.git","8ee551a402ffff816b94d8e2e3075cbf52286e8b")
 get_submodule("util_unicode","https://github.com/Silverlan/util_unicode.git","5a0ac6c02f199e42d7d38d99231503cf42e26f8a")
 get_submodule("cppbezierfit","https://github.com/Silverlan/cppbezierfit.git","f691045dfdb3313205a09ef4f460203be0a3152c")

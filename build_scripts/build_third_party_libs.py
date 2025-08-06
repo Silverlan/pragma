2bb48e53059fa6f6e6466059eaa89f1fc6a41c33
Silverlan <silverlann@gmail.com>
2025-08-06 21:19:21 +0200

    feat(windows): add os notification support

--- build_scripts/build_third_party_libs.py
+++ build_scripts/build_third_party_libs.py
@@ -531,12 +531,32 @@ if platform == "linux":
 		reset_to_commit(commit_sha)
 
 		mkdir("build",cd=True)
-		sdbus_args = ["-DCMAKE_BUILD_TYPE=Release", "-DSDBUSCPP_BUILD_LIBSYSTEMD=OFF"]
+		sdbus_args = ["-DCMAKE_BUILD_TYPE=" +build_config_tp, "-DSDBUSCPP_BUILD_LIBSYSTEMD=OFF"]
 		cmake_configure_def_toolset("..",generator,sdbus_args)
 		cmake_build(build_config_tp)
 
 		copy_prebuilt_binaries(sdbus_root +"/build/" +build_config_tp, "sdbus-cpp")
 		copy_prebuilt_headers(sdbus_root +"/include", "sdbus-cpp")
+else:
+	########## WinToast ##########
+	os.chdir(deps_dir)
+	wintoast_root = normalize_path(os.getcwd() +"/WinToast")
+	commit_sha = "9c14f6cc22739c7fd4d4a97e45ceccd8438acc62"
+	if not check_repository_commit(wintoast_root, commit_sha, "WinToast"): 
+		os.chdir(deps_dir)
+		if not Path(wintoast_root).is_dir():
+			print_msg("WinToast not found. Downloading...")
+			git_clone("https://github.com/mohabouje/WinToast.git")
+		os.chdir(wintoast_root)
+		reset_to_commit(commit_sha)
+
+		mkdir("build",cd=True)
+		sdbus_args = ["-DCMAKE_BUILD_TYPE=" +build_config_tp]
+		cmake_configure_def_toolset("..",generator,sdbus_args)
+		cmake_build(build_config_tp)
+
+		copy_prebuilt_binaries(wintoast_root +"/build/" +build_config_tp, "WinToast")
+		copy_prebuilt_headers(wintoast_root +"/include", "WinToast")
 
 ########## freetype (built in win32, sys in linux (set in cmake)) ##########
 if platform == "win32":

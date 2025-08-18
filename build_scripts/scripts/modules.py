527df9ee8ec327cdc2259f5d4acea45570a638f3
Silverlan <silverlan@pragma-engine.com>
2025-08-18 17:53:49 +0200

    build: update submodules "interfaces"

--- build_scripts/scripts/modules.py
+++ build_scripts/scripts/modules.py
@@ -8,7 +8,7 @@ curDir = os.getcwd()
 
 modules_dir = curDir +"/modules"
 os.chdir(modules_dir)
-get_submodule("interfaces","https://github.com/Silverlan/pragma_interfaces.git","eb1e2bb85cad0dcf9ca39e4aeda534ccba7deae8")
+get_submodule("interfaces","https://github.com/Silverlan/pragma_interfaces.git","a3892717997fa3909a53f13393515fe353a91f43")
 get_submodule("pr_audio_dummy","https://github.com/Silverlan/pr_audio_dummy.git","6fff8474681ffac9f365b068c20a93f4479162f1")
 get_submodule("pr_curl","https://github.com/Silverlan/pr_curl.git","c243468a77de16ae4657e257ea965d1cac05fe8e")
 get_submodule("pr_prosper_opengl","https://github.com/Silverlan/pr_prosper_opengl.git","d7d80184ed7b94657a92c793d3d5c79efdaf0197")

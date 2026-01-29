3d693e2455929f5748520c205369caf281d8d8d4
Silverlan <silverlan@pragma-engine.com>
2026-01-29 21:03:37 +0100

    build: update cmake policy version

--- cmake/pr_common.cmake
+++ cmake/pr_common.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 4.2.0)
+cmake_minimum_required(VERSION 4.1.2)
 
 function(pr_get_normalized_identifier_name BASE_NAME)
     string(REPLACE "-" "_" IDENTIFIER_UNDERSCORE ${BASE_NAME})

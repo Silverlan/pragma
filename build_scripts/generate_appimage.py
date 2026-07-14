b72389f47381154ff4b5a9ca286161ce0516cf64
Silverlan <silverlan@pragma-engine.com>
2026-07-14 13:11:28 +0200

    build(linux): add build script for generating AppImage

---
+++ build_scripts/generate_appimage.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+import os
+import shutil
+import subprocess
+from pathlib import Path
+import argparse
+
+def str2bool(v):
+	if isinstance(v, bool):
+		return v
+	if v.lower() in ('yes', 'true', 't', 'y', '1'):
+		return True
+	elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+		return False
+	else:
+		raise argparse.ArgumentTypeError('Boolean value expected.')
+
+parser = argparse.ArgumentParser(description='Pragma AppImage Generator', allow_abbrev=False, formatter_class=argparse.ArgumentDefaultsHelpFormatter, epilog="")
+
+parser.add_argument('--build-directory', help='Pragma build directory.', required=True)
+parser.add_argument('--install-directory', help='Pragma installation directory.', required=True)
+parser.add_argument('--build-tools-directory', help='Directory where additional build tools are located. Can be relative or absolute.', default='build_tools')
+parser.add_argument("--no-strip", type=str2bool, nargs='?', const=True, default=False, help="Disable stripping binaries. This may be needed on rolling release distributions.")
+
+args,unknown = parser.parse_known_args()
+args = vars(args)
+input_args = args
+
+build_directory = args["build_directory"]
+install_directory = args["install_directory"]
+build_tools_directory = args["build_tools_directory"]
+no_strip = args["no_strip"]
+
+def main():
+    appimage_base_path = Path(build_directory) / "appimage"
+    appimage_data_path = appimage_base_path / "data"
+    appimage_install_path = appimage_data_path / "usr/bin"
+    os.makedirs(appimage_data_path, exist_ok=True)
+
+    shutil.rmtree(appimage_install_path, ignore_errors=True)
+    shutil.rmtree(appimage_data_path / "usr/lib", ignore_errors=True)
+    print(f"Copying install files to {str(appimage_install_path)}...")
+    shutil.copytree(install_directory, appimage_install_path)
+
+    repo_path = Path.cwd().parent
+    
+    appimage_asset_path = Path.cwd() / "appimage"
+    shutil.copy(repo_path / "tools/pfm_executable/logo/logo_dark.png", appimage_base_path / "icon.png")
+    shutil.copy(repo_path / "tools/pfm_executable/pfm.desktop", appimage_base_path / "pfm.desktop")
+    shutil.copy(appimage_asset_path / "run.sh", appimage_base_path / "run.sh")
+
+    os.chdir(build_tools_directory)
+    if not Path("linuxdeploy-x86_64.AppImage").exists():
+        print("Fetching linuxdeploy...")
+        subprocess.run(["wget", "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"], check=True)
+        subprocess.run(["chmod", "+x", "linuxdeploy-x86_64.AppImage"], check=True)
+
+    extra_execs = []
+
+    modules_dir = Path(appimage_data_path / "usr/bin/modules")
+    if modules_dir.exists():
+        for file in modules_dir.rglob("*.so"):
+            if file.is_file():
+                extra_execs.extend(["-e", str(file)])
+
+    # Executables
+    extra_execs.extend(
+        ["-e", appimage_data_path / "usr/bin/modules/chromium/pr_chromium_subprocess"],
+        ["-e", appimage_data_path / "usr/bin/modules/chromium/chrome-sandbox"]
+    )
+
+    appdir_lib_path = str(appimage_data_path / "usr/bin/lib")
+    existing_ld_path = os.environ.get("LD_LIBRARY_PATH", "")
+
+    if existing_ld_path:
+        os.environ["LD_LIBRARY_PATH"] = f"{appdir_lib_path}:{existing_ld_path}"
+    else:
+        os.environ["LD_LIBRARY_PATH"] = appdir_lib_path
+
+    if no_strip:
+        os.environ["NO_STRIP"] = "1"
+
+    os.chdir(appimage_base_path)
+    cmd = [
+        str(Path(build_tools_directory) / "linuxdeploy-x86_64.AppImage"),
+        "--appdir",
+        "data",
+        "--executable",
+        "data/usr/bin/pfm",
+    ]
+
+    cmd.extend(extra_execs)
+
+    cmd.extend(
+        [
+            "--desktop-file",
+            str(appimage_base_path / "pfm.desktop"),
+            "--icon-file",
+            str(appimage_base_path / "icon.png"),
+            "--custom-apprun",
+            str(appimage_base_path / "run.sh"),
+            "--output",
+            "appimage",
+        ]
+    )
+
+    print(f"Launching linuxdeploy with command {cmd}")
+    try:
+        subprocess.run(cmd, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"linuxdeploy failed with exit code {e.returncode}")
+        exit(e.returncode)
+
+if __name__ == "__main__":
+    main()

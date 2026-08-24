10b98b7f3e18b00fbcf9a3098173954b782c4b39
Silverlan <silverlan@pragma-engine.com>
2026-08-24 07:35:10 +0200

    build: update submodule "pfm"

--- Addons.cmake
+++ Addons.cmake
@@ -124,7 +124,7 @@ function(pr_install_git_release IDENTIFIER BASE_URL BASE_DIR TAG_NAME)
 endfunction()
 
 if(PRAGMA_WITH_PFM)
-    pr_install_git_repository(pfm "https://github.com/Silverlan/pfm.git" "de4d9563ca8b8e8b377f6b7116193bb9c11ebcdf" "addons/filmmaker")
+    pr_install_git_repository(pfm "https://github.com/Silverlan/pfm.git" "ce151fd34a52e03280ae8b8e2c370513df566fab" "addons/filmmaker")
     pr_install_git_repository(model_editor "https://github.com/Silverlan/pragma_model_editor.git" "149f48b11af5dde304c7dd432ff6a456cdb4f450" "addons/tool_model_editor")
 
     pr_install_git_repository(pfm_demo_living_room "https://github.com/Silverlan/pfm_demo_living_room.git" "4cbecad4a2d6f502b6d9709178883678101f7e2c" "addons/pfm_demo_living_room")
